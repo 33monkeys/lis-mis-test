703a0bce9d16fc1ae089ecc2f407ea56beee1635
a.lisnyi <a.lisnyi@netrika.ru>
2015-04-28 18:00:11 +0300

    New lis order workflow and validation

--- Integration/Common/FhirHelper.cs
+++ Integration/Common/FhirHelper.cs
@@ -6,10 +6,18 @@ namespace Lis.Test.Integration.Common
     {
         public static ResourceReference CreateReference(Resource resource)
         {
-            var reference = string.Format("{0}/{1}", resource.TypeName, resource.Id);
+            var reference = string.Format("/{0}/{1}", resource.TypeName, resource.Id);
             return new ResourceReference
             {
                 Reference = reference
+            };
+        }
+
+        public static ResourceReference CreateBundleReference(Resource resource)
+        {
+            return new ResourceReference
+            {
+                Reference = resource.Id,
             };
         }
     }
